1c5cea652f62d2dcdf8b9530612f5efe358e96dc
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 18:51:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,15 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-bot.on("ready", () => {
-        console.log(`${bot.user.username} has started! With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
-        bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`);
-    
-    });
-    
-        
-        bot.on("guildCreate", guild => {
-            console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-            bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`);
-          });
-         
-          bot.on("guildDelete", guild => {
-            console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-            bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`);
-          });
-//cmds
-
-bot.on('message', message => {
-    if (message.content === '$roll') {
-
-        
-      
-        
-        
-   message.channel.send('bob');
-    }
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-bot.on('message', message => {
+client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
 
-
-bot.login(process.env.BOT_TOKEN);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
