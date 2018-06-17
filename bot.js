e97db39eb5e5ae49dc1f32b7b20edb34a2943a4d
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 17:35:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,30 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
-client.on('ready', () => {
-    console.log('I am ready!');
+
+bot.on("ready", () => {
+        bot.user.setGame(`$help | Playing With ${bot.users.size} Users!`)
+    
+});
+    
+        
+bot.on("guildCreate", guild => {
+         bot.user.setGame(`$help | Playing With ${bot.channels.size} Users!`);
+});
+         
+bot.on("guildDelete", guild => {
+         bot.user.setGame(`$help | Playing With ${bot.channels.size} Users!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+bot.on("message", message => {
+     if (message.content === '$ping') {
+       message.reply("pong");
+       }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return message.channel.send(`Hello! My prefix is "$"! Invite me to your server with the following link {}} Or join our server main! {https://discord.gg/5Du3jDt} Thanks!`);
+
+bot.login(process.env.BOT_TOKEN);
