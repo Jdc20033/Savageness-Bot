ef6c3a6ddaeb9df0a0155d743076e2c73bb0bb44
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 19:46:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,5 +25,63 @@ bot.on("message", async message => {
 
 });
 
+//8ball
 
+var sayings = ["It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes, definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful"];
+
+
+
+var result = Math.floor((Math.random() * sayings.length) + 0);
+
+let embed = new Discord.RichEmbed()
+.setColor("#FF0000")
+.addField(":8ball: Your answer is", sayings[result])
+
+message.channel.send(embed);
+
+}
+
+//avatar
+let msg = await message.channel.send("Loading avatar...")
+      let target = message.mentions.users.first() || message.author;
+   
+   
+      await message.channel.send({files: [
+      {   
+          attachment: target.displayAvatarURL,
+          name: "avatar.png"
+      }
+       ]});
+
+       msg.delete();
+    }
+//roll
+    var result = Math.floor((Math.random() * 100) + 1);
+       
+        let embed = new Discord.RichEmbed()
+        .setColor("#FF0000")
+        .addField(":game_die: You rolled a: ", + result);
+        
+   message.channel.send(embed);
+ 
+}    
 bot.login(process.env.BOT_TOKEN);
