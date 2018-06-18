7aa2c9d84156bb7d89dd39f90b0c40b7d083dc0b
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 20:40:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,7 +130,7 @@ if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)
     if(!reason) reason = "No reason provided";
     
     // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
+    member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
     message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
