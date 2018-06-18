accf2fff9c3692481cab7fb741c09a52106d5e1e
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 21:20:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,9 @@ bot.on("message", message => {
       return message.reply("Please mention a valid member of this server");
     if(!member.kickable) 
       return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-        member.kick()
+        let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+        member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
     message.reply(`${member.user.tag} has been kicked by ${message.author.tag}.`);
 };
