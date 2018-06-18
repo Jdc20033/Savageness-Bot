749c64358f4278d8131ea36323f8f158c2771aa3
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 20:29:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ let embed = new Discord.RichEmbed()
               .setColor("#FF0000")
               .addField("Full Username", `ØneŁife`)
               .addField("ID", "323989674399891459")
-              .addField("Created At", "6/17/2018");      
+              .addField("Bot Created At", "6/17/2018");      
 let embed2 = new Discord.RichEmbed()
               .setDescription("Commands!")
               .setColor("#FF0000")
@@ -108,4 +108,26 @@ let embed2 = new Discord.RichEmbed()
               message.reply("Sent. Check your direct messages.");
 }
 });    
-bot.login(process.env.BOT_TOKEN);
+//kick
+bot.on("message", async message => {
+
+    if (message.content === "$kick") {
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the proper roles!");
+    
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("You did not specify a user!");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+   
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick that user. Reason: ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag}. Reason: ${reason}`);
+  }
+});
+ bot.login(process.env.BOT_TOKEN);
