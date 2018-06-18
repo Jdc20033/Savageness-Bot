2cb7dea6f67514556453f858ef547c5e131a9c70
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 20:56:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,26 +110,15 @@ let embed2 = new Discord.RichEmbed()
 bot.on("message", message => {
 
     if (message.content === "$kick") {
-if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first()
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    
-    
-    // Now, time for a swift kick in the nuts!
-    member.kick()
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag}.`);
-
-  }
+var member= message.mentions.members.first();
+        // Kick
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
 });
  bot.login(process.env.BOT_TOKEN);
