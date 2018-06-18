3567d9eabe51a2819547eddc37a286276cf3cfef
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 19:26:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+  	client.on('message', message =>{
+    if (message.content === 'ping'){
+    	message.channel.sendMessage('pong');
   	}
 });
 
