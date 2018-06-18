241a3024f88fc2b8edbc1b5326f083853bc6f240
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 19:42:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,29 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 
-  	client.on('message', message => {
-    if(message.content === '.on') {
-        console.log('Received #' + message.id + ': ' + message.content);
-        message.channel.send('Testing Bot is now Online, Greetings, ' + message.author.username)
-        .then(message => console.log('Sent #' + message.id + ': ' + message.content))
-        .catch(console.error);
-    }
-});
+    bot.on("ready", () => {
+        console.log(`${bot.user.username} has started! With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
+        bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`)
     
+    });
+    
+        
+        bot.on("guildCreate", guild => {
+            console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+            bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`);
+          });
+         
+          bot.on("guildDelete", guild => {
+            console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+            bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`);
+          });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return message.channel.send(`Hello! My prefix is "$"! Invite me to your server with the following link {https://discordapp.com/oauth2/authorize?client_id=458018248190066730&permissions=8&scope=bot}} Or join our server main! {https://discord.gg/5Du3jDt} Thanks!`);
+
+});
+
+
+bot.login(process.env.BOT_TOKEN);
