599aa5f4ee1e27413022cd5705e62aee4c0443d0
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 19:28:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,15 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-  	client.on('message', message =>{
-    if (message.content === 'ping'){
-    	message.channel.sendMessage('pong');
-  	}
+  	client.on('message', message => {
+    if(message.content === '.on') {
+        console.log('Received #' + message.id + ': ' + message.content);
+        message.channel.send('Testing Bot is now Online, Greetings, ' + message.author.username)
+        .then(message => console.log('Sent #' + message.id + ': ' + message.content))
+        .catch(console.error);
+    }
 });
+    
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
