20a54205a5f778f6e1bccaa22e584c910eaf7e10
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 20:55:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,12 +112,10 @@ bot.on("message", message => {
     if (message.content === "$kick") {
 if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
-        let messageArray = message.content.split(/\s+/g);
-        let args = messageArray.slice(1);
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    let member = message.mentions.members.first()
     if(!member)
       return message.reply("Please mention a valid member of this server");
     if(!member.kickable) 
@@ -125,13 +123,12 @@ if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)
     
     // slice(1) removes the first part, which here should be the user mention or ID
     // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
+    
     
     // Now, time for a swift kick in the nuts!
-    member.kick(reason)
+    member.kick()
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag}.`);
 
   }
 });
