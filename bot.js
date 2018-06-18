32540a517488c19e82f44d86ef3d0e9d34cbfa3d
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 22:22:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,24 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({disableEveryone: true});
-const prefix = ('$');
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+
+
+bot.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  const prefix = ("$");
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
 
 bot.on("ready", () => {
