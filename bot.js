988e04d312be4c543f684c5f2230c024680aed29
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 20:21:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,5 +63,5 @@ bot.on("message", async message => {
 const m = await message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
       }
-        
+});       
 bot.login(process.env.BOT_TOKEN);
