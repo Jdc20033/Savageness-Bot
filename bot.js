e29502ffdfbfcf4f45adac7852df88c20d5c284a
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 21:42:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,33 +109,35 @@ let embed2 = new Discord.RichEmbed()
 //kick
 bot.on("message", message => {
     if (message.content === "$kick") {
+       if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the proper roles!");
        let messageArray = message.content.split(/\s+/g);
        let commands = messageArray[0];
-       let args = messageArray.slice(1);
-        if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-      let member = message.mentions.members.first()
-      if(!member)
-      return message.reply("Please mention a valid member of this server");
+       let args = messageArray.slice(1); 
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("You did not specify a user!");
     if(!member.kickable) 
       return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-        let reason = args.join(" ");
-    if(!reason) reason = "No reason provided";
-        member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag}.`);
-};
+   
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick that user. Reason: ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag}. Reason: ${reason}`);
+
+  }
 });
 //say
 bot.on("message", message => {
     if (message.content === "$say") {
-    let messageArray = message.content.split(/\s+/g);
-    let commands = messageArray[0];
-    let args = messageArray.slice(1);
+    const fs = require("fs");
+    const Discord = require("discord.js");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
-    
-}
+    }
 });
 bot.login(process.env.BOT_TOKEN);
