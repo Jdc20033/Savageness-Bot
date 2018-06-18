0a02cca7498c40a68cead43eb575f5372169d20c
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 19:43:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,19 @@ const bot = new Discord.Client({disableEveryone: true});
 
     bot.on("ready", () => {
         console.log(`${bot.user.username} has started! With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
-        bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`)
+        bot.user.setGame(`$help | Playing With ${bot.channels.size} Servers!`)
     
     });
     
         
         bot.on("guildCreate", guild => {
             console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-            bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`);
+            bot.user.setGame(`$help | Playing With ${bot.channels.size} Servers!`);
           });
          
           bot.on("guildDelete", guild => {
             console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-            bot.user.setActivity(`$help | Playing With ${bot.channels.size} Servers!`);
+            bot.user.setGame(`$help | Playing With ${bot.channels.size} Servers!`);
           });
 
 bot.on("message", async message => {
