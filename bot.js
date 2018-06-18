05e7286464227462ad670f597d7ed4d3e8b2aa35
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-17 22:03:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,7 +110,7 @@ let embed2 = new Discord.RichEmbed()
 //say
 bot.on("message", message => {
     if (message.content === "$say") {
-    const sayMessage = args.join(" ");
+    const sayMessage = args.join("");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
