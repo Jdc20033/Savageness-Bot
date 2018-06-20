d6bf963570685c9ca937196ce948ab6d67a40f74
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-20 04:08:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const { prefix } = require('./config.json');
-const client = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('Ready!');
+});
 
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
