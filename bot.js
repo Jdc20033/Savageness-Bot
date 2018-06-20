f5141fef8c339fce410f113ef84319888225e99f
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-20 15:14:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,13 +102,58 @@ bot.on('message', async message => {
 		});
 	}
         else if (command === "say") {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage);
+                const sayMessage = args.join(" ");
+                message.delete().catch(O_o=>{}); 
+                message.channel.send(sayMessage);
         }
         else if (command === "invite") {
 		message.reply("Here's my invite! https://discordapp.com/api/oauth2/authorize?client_id=458029145700433924&permissions=474344695&scope=bot");
 	}
+	else if (command === "mute") {
+                if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the proper roles!");
+  
+              let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+              if(!toMute) return message.channel.send("You did not specify a user!");
+            
+              if(toMute.id === message.author.id) return message.channel.send("You cant mute yourself!");
+              if(toMute.highestRole.position >= message.member.highestRole.position) return message.channel.send("I cannot mute this user! Do they have a higher role? Do I have mute permissions?");
+  
+              let role = message.guild.roles.find(r => r.name === "Muted");
+             if(!role) {
+              try{
+               role =  await message.guild.createRole({
+                       name: "Muted",
+                       color: "#4b4b4b",
+                       permissions: []                   
+                      });
+  
+                      message.guild.channels.forEach(async (channel, id) => {
+                         await channel.overwritePermissions(role, {
+                                 SEND_MESSAGES: false,
+                                 ADD_REACTIONS: false
+                          });
+                     });
+              } catch(e) {
+                console.log(e.stack);
+              }
+          }
+          if(toMute.roles.has(role.id)) return message.channel.send("This user is already muted!");
+       
+        bot.mutes[toMute.id] = {
+            guild: message.guild.id,
+            time: Date.now() + parseInt(args[1]) * 1000
+        }
+
+        await toMute.addRole(role);  
+    
+        fs.writeFile("./mutes.json", JSON.stringify(bot.mutes, null, 4), err => {
+            if(err) throw err;
+            message.channel.send(`Muted ${toMute.user.tag}.` );
+    });
+}
+	
+	
+	
 	else if (command === "help") {
 	       let embed = new Discord.RichEmbed()
               .setAuthor("ØneŁife")
