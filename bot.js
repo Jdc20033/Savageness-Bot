7a0dfd05aeb54d70bbe07fca49d85222c1b28ff9
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-20 04:48:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,22 @@ const bot = new Discord.Client();
 bot.on('ready', () => {
 	console.log('Ready!');
 });
+bot.on("ready", () => {
+        console.log(`${bot.user.username} has started! With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
+        bot.user.setGame(`in ${bot.guilds.size} servers! | $help`);
+    
+});
+    
+        
+bot.on("guildCreate", guild => {
+         console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+         bot.user.setGame(`in ${bot.guilds.size} servers! | $help`);
+});
+         
+bot.on("guildDelete", guild => {
+          console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+          bot.user.setGame(`in ${bot.guilds.size} servers! | $help`);
+});
 
 bot.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
