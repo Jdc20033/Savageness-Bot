e2364a015ee10660b5b91f7ea0869b319ad40d63
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-20 18:27:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,7 +146,7 @@ bot.on('message', async message => {
 
 await toMute.addRole(role);
 
-fs.writeFile('mutes.json', JSON.stringify(bot.mutes), err => {
+fs.writeFile ("mutes.json", JSON.stringify(bot.mutes), err => {
     if(err) throw err;
     message.channel.send("I have muted this user!");       
 });
