c1f7eb583c2bb732b37e4da8c0279bb446d23471
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 18:40:42 -0500

    Update prefix.js

--- cmds/prefix.js
+++ cmds/prefix.js
@@ -1,14 +1,1 @@
-module.exports.run = async (bot, message, args) => {
-guildConf[message.guild.id].prefix = args[0];
-	if (!guildConf[message.guild.id].prefix) {
-		guildConf[message.guild.id].prefix = config.prefix; // If you didn't specify a Prefix, set the Prefix to the Default Prefix
-	}
-     fs.writeFile('./storages/guildConf.json', JSON.stringify(guildConf, null, 2), (err) => {
-     	if (err) console.log(err)
-	})
-  }
-}
-});
-module.exports.help = {
- name: "prefix"
-}
+
