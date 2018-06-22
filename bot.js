14c52896b309bdb3b705d643614e99d1cd7481c3
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 17:36:35 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ bot.setInterval(() => {
             }       
         }, 60000);
  
-{
+module.exports.run = async(bot, message, args) => {
 let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
 if(!prefixes[message.guild.id]){
@@ -60,7 +60,7 @@ if(!prefixes[message.guild.id]){
 
 let prefix2 = prefixes[message.guild.id].prefixes
 console.log(prefix);
-
+}
 
 bot.on("ready", () => {
    console.log(`${bot.user.username} has started! With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
