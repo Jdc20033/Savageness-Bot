33611a6aa73151760a863368ec408afd7ccdb575
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 17:01:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,37 @@ fs.readdir("./cmds/", (err, files) => {
        });
     });      
 
+    bot.on("ready", async () => {
+    
+    try {
+        let link = await ""
+        console.log(link);
+    } catch(e) {
+        console.log(e.stack);
+    }
+    
+       bot.on("ready", () => {
+           console.log(`${bot.user.username} has started! With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
+           bot.user.setGame(`Playing with ${bot.users.size} users! || $help`)
+    
+         });
+    
+        
+        bot.on("guildCreate", guild => {
+            console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+            bot.user.setGame(`Playing with ${bot.users.size} users! || $help`);
+          });
+         
+          bot.on("guildDelete", guild => {
+            console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+            bot.user.setGame(`Playing with ${bot.users.size} users! || $help`);
+          });
+
+    
+
+
+
+
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return message.channel.send(`Hello! My prefix is "$"! Invite me to your server with the following link {https://discordapp.com/oauth2/authorize?client_id=458018248190066730&permissions=8&scope=bot}} Or join our server main! {https://discord.gg/5Du3jDt} Thanks!`);
