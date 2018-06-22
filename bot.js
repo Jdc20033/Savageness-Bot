5f223eb2e484a694cc138773757802dba4d1aa19
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 16:04:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,7 +210,55 @@ let embed2 = new Discord.RichEmbed()
               message.reply("Sent. Check your direct messages.");
 
 
-}
-});                     
+        }
+        else if (command === "8ball") {
+        var sayings = ["It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes, definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"];
 
+        var result = Math.floor((Math.random() * sayings.length) + 0);
+
+        let embed = new Discord.RichEmbed()
+        .setColor("#FF0000")
+        .addField(":8ball: Your answer is", sayings[result])
+
+        message.channel.send(embed);
+
+        }                  
+        else if (command === "coinflip") {
+        const result = Math.floor((Math.random() * 2) + 1);
+        	if (result == 1) {
+        		message.channel.send("The coin landed on heads!");
+        	} else if (result == 2) {
+        		message.channel.send("The coin landed on tails! ");
+        	}
+	}
+        else if (command === "") {
+	var result = Math.floor((Math.random() * 100) + 1);
+       
+        let embed = new Discord.RichEmbed()
+        .setColor("#FF0000")
+        .addField(":game_die: You rolled a: ", + result);
+        
+   message.channel.send(embed);
+ 
+       }
+});
 bot.login(process.env.BOT_TOKEN);
