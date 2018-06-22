959cc136b79d66e5d493760e79a422f594bcd53b
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 16:56:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,4 +25,22 @@ fs.readdir("./cmds/", (err, files) => {
           bot.commands.set(props.help.name, props);
        });
     });      
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return message.channel.send(`Hello! My prefix is "$"! Invite me to your server with the following link {https://discordapp.com/oauth2/authorize?client_id=458018248190066730&permissions=8&scope=bot}} Or join our server main! {https://discord.gg/5Du3jDt} Thanks!`);
+
+    let messageArray = message.content.split(/\s+/g);
+    let commands = messageArray[0];
+    let args = messageArray.slice(1);
+    let input = messageArray[1];
+
+    if(!commands.startsWith(prefix)) return;
+    
+
+    let cmd = bot.commands.get(commands.slice(prefix.length));
+    if(cmd) cmd.run(bot, message, args, input);
+  
+});
+
 bot.login(process.env.BOT_TOKEN);
