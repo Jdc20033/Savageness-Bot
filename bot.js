aa9c8410b865cb4155c17c6432211e61d2f01a64
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-23 18:29:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,6 @@ bot.setInterval(() => {
                 }
             }       
         }, 60000)
-    }); 
  
 bot.on("ready", () => {
    console.log(`${bot.user.username} has started! With ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
