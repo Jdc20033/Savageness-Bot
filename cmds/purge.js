8d56d75eb848af51f0593dc4164f0c826b4fa252
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 19:52:36 -0500

    Update purge.js

--- cmds/purge.js
+++ cmds/purge.js
@@ -1,14 +1,14 @@
 module.exports.run = async (bot, message, args, input) => {
 
-const user = (msg.mentions.users.first() || bot.users.get(args[0]) || null);
-  const amount = !!user ? parseInt(msg.content.split(" ")[2], 10) : parseInt(msg.content.split(" ")[1], 10);
-  if (!amount) return msg.edit("Must specify an amount to delete!").then(msg.delete(2000));
-  if (!amount && !user) return msg.edit("Must specify a user and amount, or just an amount, of messages to purge!").then(msg.delete(2000));
-  await msg.delete();
-  let messages = await msg.channel.messages.fetch({limit: 100});
+const user = (message.mentions.users.first() || bot.users.get(args[0]) || null);
+  const amount = !!user ? parseInt(message.content.split(" ")[2], 10) : parseInt(message.content.split(" ")[1], 10);
+  if (!amount) return message.edit("Must specify an amount to delete!").then(message.delete(2000));
+  if (!amount && !user) return message.edit("Must specify a user and amount, or just an amount, of messages to purge!").then(message.delete(2000));
+  await message.delete();
+  let messages = await message.channel.messages.fetch({limit: 100});
   if(user) {
     messages = messages.array().filter(m=>m.author.id === user.id);
-    bot.log("log", "Purge Amount", msg.author, "Amount: " + amount);
+    bot.log("log", "Purge Amount", message.author, "Amount: " + amount);
     messages.length = amount;
   } else {
     messages = messages.array();
