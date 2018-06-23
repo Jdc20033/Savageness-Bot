58713de763546e7a6672d1b09bb9496e6b9a7b08
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 20:03:37 -0500

    Update purge.js

--- cmds/purge.js
+++ cmds/purge.js
@@ -1,8 +1,8 @@
 module.exports.run = async (bot, message, args, input) => {
 
-const deleteCount = args([0] + 1);
+const deleteCount = parseInt(args[0], 10 + 1);
     
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+    if(!deleteCount || deleteCount < 1 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
