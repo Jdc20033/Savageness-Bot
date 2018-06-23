8540696144173b454a68b6d7d014af3b11a8a3b5
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-23 17:59:43 -0500

    Update mute.js

--- cmds/mute.js
+++ cmds/mute.js
@@ -37,7 +37,7 @@ module.exports.run = async (bot, message, args) => {
 
         await toMute.addRole(role);  
     
-        fs.appendFile('./cmds'+/mutes.json,'bot.mutes','utf8',(err)=>{if(err) throw err; console.log('data was appended to area`)})
+        fs.appendFile('./cmds/'+mutes.json,'appending this text to file','utf8',(err)=>{if(err) throw err; console.log('data was appended to '+mutes.json)})
             message.channel.send(`Muted ${toMute.user.tag}.` );
     });
 }
