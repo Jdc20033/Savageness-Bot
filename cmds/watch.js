3f350a1add4e922cd7ab468e41ec28077e2eb680
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 20:24:20 -0500

    Update watch.js

--- cmds/watch.js
+++ cmds/watch.js
@@ -5,13 +5,13 @@ module.exports.run = async(bot, message, args) => {
   const channel = (bot.channels.get(args[0]) || message.channel);
   if(watched.has(channel.id)) {
     watched.get(channel.id).stop();
-    message.edit("Channel Watch Stopped on #"+channel.name);
+    message.channel.send("Channel Watch Stopped on #"+channel.name);
     return watched.delete(channel.id);
   }
   
   message.edit("I have started watching #"+channel.name);
   const collector = channel.createMessageCollector(()=>true);
-  collector.on("collect", (collected, collector) => console.log(`[WATCHED][${collected.author.username}][#${collected.channel.name}]${collected.content}`));
+  collector.on("collect", (collected, collector) => console.log(`[Watched][${collected.author.username}][#${collected.channel.name}]${collected.content}`));
   watched.set(channel.id, collector);
 };
 module.exports.help = {
