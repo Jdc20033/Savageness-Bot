d666096b99f6a61b731dcb01a462e7d09bad292d
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 20:01:28 -0500

    Update purge.js

--- cmds/purge.js
+++ cmds/purge.js
@@ -1,21 +1,14 @@
 module.exports.run = async (bot, message, args, input) => {
 
-const user = (message.mentions.users.first() || bot.users.get(args[0]) || null);
-  const amount = !!user ? parseInt(message.content.split(" ")[2], 10) : parseInt(message.content.split(" ")[1], 10);
-  if (!amount) return message.channel.send("Must specify an amount to delete!").then(message.delete(2000));
-  if (!amount && !user) return message.channel.send("Must specify a user and amount, or just an amount, of messages to purge!").then(message.delete(2000));
-  await message.delete();
-  let messages = await message.channel.messages.fetch({limit: 100});
-  if(user) {
-    messages = messages.array().filter(m=>m.author.id === user.id);
-    bot.log("log", "Purge Amount", message.author, "Amount: " + amount);
-    messages.length = amount;
-  } else {
-    messages = messages.array();
-    messages.length = amount + 1;
+const deleteCount = input(" " + 1);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
-  messages.map(async m => await m.delete().catch(console.error));
-};
 
 module.exports.help = {
     name: "purge"
