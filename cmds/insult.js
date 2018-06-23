5a62b523ba8c3fce5ec503f6ae5cfbb1f2292c1e
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-22 19:32:35 -0500

    Update insult.js

--- cmds/insult.js
+++ cmds/insult.js
@@ -25,10 +25,10 @@ var sayings = ["I'd like to see things from your point of view, but my head does
 
 var result = Math.floor((Math.random() * sayings.length) + 0);
 
-let target = message.mentions.users.first() || message.author;
-if(!target) return message.channel.send(target, sayings[result]);
+let target = message.mentions.users.first()
+if(!target) return message.channel.send(sayings[result]);
 
-message.channel.send(sayings[result]);
+message.channel.send(target, sayings[result]);
 
 }
 
