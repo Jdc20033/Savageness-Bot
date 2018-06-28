a4be87f7a0071a54f6089de31f90c6e471948768
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-28 16:31:37 -0500

    Updates

--- cmds/poll.js
+++ cmds/poll.js
@@ -1,8 +1,8 @@
 module.exports.run = async (bot, message, args) => {
     
-    message.react('👍')
-    message.react('👎')
-    message.react('🤔')
+    message.react('U+1F44D')
+    message.react('U+1F44E')
+    message.react('U+1F914')
 
 }
 module.exports.help = {
