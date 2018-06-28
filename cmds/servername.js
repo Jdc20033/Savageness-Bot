947ddc0d3c90f026d029ee7d03f71cbc732ef5a8
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-28 16:24:40 -0500

    Updates

--- cmds/servername.js
+++ cmds/servername.js
@@ -1,9 +1,9 @@
 module.exports.run = async (bot, message, args) => {
-if(message.author.hasPermission('MANAGE_SERVER')) return message.channel.send("You don't have the proper roles!");
+if(message.user.hasPermission('MANAGE_SERVER')) return message.channel.send("You don't have the proper roles!");
 
 const input = args.join(" ");
 guild.setName(input)
 }
 module.exports.help = {
-    name: "poll"
+    name: "setservername"
 }
