87247f39b82de012ab5e4ad599ba0f587afa87db
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-29 16:10:14 -0500

    Updates

--- cmds/restart.js
+++ cmds/restart.js
@@ -3,8 +3,8 @@ module.exports.run = async(bot, message, args) => {
 
     if(!message.member.id === ('323989674399891459')) return message.channel.send('Restarting Savage Bot!')
     else message.channel.send("Restarting bot!")
-    console.log("rs");
-
+    console.log("Requested restart!");
+    bot.destroy()
 
 }
     module.exports.help = {
