03a4c7ba33e55c0d4839233c06f1e7ae59e224a7
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-29 16:23:07 -0500

    Updates

--- cmds/restart.js
+++ cmds/restart.js
@@ -1,7 +1,7 @@
 module.exports.run = async(bot, message, args) => {
 
 
-    if (message.author.id !== '323989674399891459') return message.channel.send('This is a owner only permission.')
+    if (message.author.id !== '523989674399891459') return message.channel.send('This is a owner only permission.')
     else if (message.channel.send("Restarting bot!"))
     console.log("Requested restart!");
     bot.destroy()
