d1ae75491022ed18514a4dfb5b9ce0a4faa07de9
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-29 17:56:05 -0500

    Updates

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -1,17 +1,17 @@
 module.exports.run = async(bot, message, args) => {
 
 
-const links = ["https://media.giphy.com/media/VdcYTcb2nRSIU/giphy.gif",
-"http://media.giphy.com/media/4cz3D8Yhyzcg8/giphy.gif",
-"http://media.giphy.com/media/cS9lGF8gIBdQs/giphy.gif",
-"http://media.giphy.com/media/MZDXTFeePBia4/giphy.gif",
-"http://media.giphy.com/media/9umH7yTO8gLYY/giphy.gif",
-"http://media.giphy.com/media/RXbc4wDSqYs3m/giphy.gif",
-"http://media.giphy.com/media/FEq9xWq9fauLC/giphy.gif",
-"https://i.makeagif.com/media/5-08-2014/ircXAq.gif",
-"https://i.makeagif.com/media/7-29-2014/9_vBku.gif",
-"http://i.imgur.com/IoJ4AGS.gif",
-"http://i.imgur.com/JwmiE.gif"];
+const links = [" https://media.giphy.com/media/VdcYTcb2nRSIU/giphy.gif",
+" http://media.giphy.com/media/4cz3D8Yhyzcg8/giphy.gif",
+" http://media.giphy.com/media/cS9lGF8gIBdQs/giphy.gif",
+" http://media.giphy.com/media/MZDXTFeePBia4/giphy.gif",
+" http://media.giphy.com/media/9umH7yTO8gLYY/giphy.gif",
+" http://media.giphy.com/media/RXbc4wDSqYs3m/giphy.gif",
+" http://media.giphy.com/media/FEq9xWq9fauLC/giphy.gif",
+" https://i.makeagif.com/media/5-08-2014/ircXAq.gif",
+" https://i.makeagif.com/media/7-29-2014/9_vBku.gif",
+" http://i.imgur.com/IoJ4AGS.gif",
+" http://i.imgur.com/JwmiE.gif"];
 
 var result = Math.floor((Math.random() * links.length) + 0);
 
