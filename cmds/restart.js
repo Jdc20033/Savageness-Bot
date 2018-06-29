7f22a516c6ae4f4d0181ada4313223a1aea757de
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-29 16:21:30 -0500

    Updates

--- cmds/restart.js
+++ cmds/restart.js
@@ -1,11 +1,12 @@
 module.exports.run = async(bot, message, args) => {
 
 
-    if(message.author.id === ('023989674399891459')) return message.channel.send('Restarting Savage Bot!')
-    else message.channel.send("Restarting bot!")
+    if (message.author.id !== '323989674399891459') return message.channel.send('This is a owner only permission.')
+    else if (message.channel.send("Restarting bot!"))
     console.log("Requested restart!");
     bot.destroy()
-
+    
+    bot.login(process.env.BOT_TOKEN);
 }
     module.exports.help = {
         name: "restart"
