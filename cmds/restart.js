388cba22d1e4177f068b43e2a6845134950408b9
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-29 16:30:58 -0500

    Updates

--- cmds/restart.js
+++ cmds/restart.js
@@ -1,13 +1,18 @@
 module.exports.run = async(bot, message, args) => {
-
-
+    
+    
     if (message.author.id !== '323989674399891459') return message.channel.send('This is a owner only permission.')
     else if (message.channel.send("Restarting bot!"))
     console.log("Requested restart!");
     bot.destroy()
-    
-    bot.login(process.env.BOT_TOKEN);
 }
+setTimeout(function() {
+    bot.login(process.env.BOT_TOKEN);
+}, 180000); 
+
+    
+
+    
     module.exports.help = {
         name: "restart"
     }
