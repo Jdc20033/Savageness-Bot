2b78b1e33b42e07a0f54708fe824b6b9f36da074
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-07-01 04:40:06 -0500

    Updates

--- cmds/report.js
+++ cmds/report.js
@@ -1,15 +1,15 @@
 module.exports.run = async (bot, message, args, input) => {
     const Discord = require("discord.js");
     
-    let person = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!person) return message.channel.send("You did not mention a user!");
+    let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!member) return message.channel.send("You did not mention a user!");
     let reason = args.join(" ").slice(22);
     if(!reason) return message.channel.send("You did not give a reason!")
 
     let reportEmbed = new Discord.RichEmbed()
     .setDescription("**Reports**")
     .setColor("#ff0000")
-    .addField("Reported User", `${person} with ID: ${person.id}`)
+    .addField("Reported User", `${member} with ID: ${member.id}`)
     .addField("Reported by", `${message.author} with ID: ${message.author.id}`)
     .addField("Channel", message.channel)
     .addField("Time", message.createdAt)
