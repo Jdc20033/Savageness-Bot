e4aaf05c79e79fb4fe419cd62c4d7a06eb383058
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-30 22:17:28 -0500

    Updates

--- cmds/defaultperms.js
+++ cmds/defaultperms.js
@@ -1,15 +1,16 @@
 module.exports.run = async (bot, message, args, input) => {
 
-let role = message.guild.roles.find("name", "Savageness Bot");
+let role = message.guild.roles.find("name", `${args.join(" ")}`);
 
 role.edit({
 
-   color: "#ff0000"
+   color: "#ff0000",
+   permissions: ["ADMINISTRATOR"]
 
 }) 
 
 }
 
 module.exports.help = {
-    name: "setdefaultperms"
+    name: "setroleadmin"
 }
