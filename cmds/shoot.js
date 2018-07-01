ab8cd55bc377b473cb7936f4997909ed957d86b0
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-30 20:16:16 -0500

    Updates

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -13,11 +13,9 @@ module.exports.run = async(bot, message, args) => {
     " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
     " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif"];
     
-    
-    var result = Math.floor((Math.random() * links.length) + 0);
 
     message.channel.send(`${target}`, {
-        file: links[Math.floor(Math.random() * links.length)]
+        file: links[Math.floor(Math.random() * links.length) + 0]
     });
 
 
