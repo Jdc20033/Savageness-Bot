95df04a566ef72462d44ed4cad818e7651d825fc
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-30 19:28:46 -0500

    Updates

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -1,30 +1,36 @@
 module.exports.run = async(bot, message, args) => {
+    let target = message.mentions.users.first() || message.author;
+
+    const links = [" https://media.giphy.com/media/VdcYTcb2nRSIU/giphy.gif",
+    " http://media.giphy.com/media/4cz3D8Yhyzcg8/giphy.gif",
+    " http://media.giphy.com/media/cS9lGF8gIBdQs/giphy.gif",
+    " http://media.giphy.com/media/MZDXTFeePBia4/giphy.gif",
+    " http://media.giphy.com/media/9umH7yTO8gLYY/giphy.gif",
+    " http://media.giphy.com/media/RXbc4wDSqYs3m/giphy.gif",
+    " http://media.giphy.com/media/FEq9xWq9fauLC/giphy.gif",
+    " https://i.makeagif.com/media/5-08-2014/ircXAq.gif",
+    " https://i.makeagif.com/media/7-29-2014/9_vBku.gif",
+    " http://i.imgur.com/IoJ4AGS.gif",
+    " http://i.imgur.com/JwmiE.gif"];
+    
+    var result = Math.floor((Math.random() * links.length) + 0);
+
+    if(!target) message.reply({files: [
+        {
+            attachment: links[result],
+            name: "meme.png"
+        }
+    ]});
+
+    await message.channel.send({files: [
+        {   
+            attachment: target.links[result],
+            name: "meme.png"
+        }
+         ]});
 
 
-const links = [" https://media.giphy.com/media/VdcYTcb2nRSIU/giphy.gif",
-" http://media.giphy.com/media/4cz3D8Yhyzcg8/giphy.gif",
-" http://media.giphy.com/media/cS9lGF8gIBdQs/giphy.gif",
-" http://media.giphy.com/media/MZDXTFeePBia4/giphy.gif",
-" http://media.giphy.com/media/9umH7yTO8gLYY/giphy.gif",
-" http://media.giphy.com/media/RXbc4wDSqYs3m/giphy.gif",
-" http://media.giphy.com/media/FEq9xWq9fauLC/giphy.gif",
-" https://i.makeagif.com/media/5-08-2014/ircXAq.gif",
-" https://i.makeagif.com/media/7-29-2014/9_vBku.gif",
-" http://i.imgur.com/IoJ4AGS.gif",
-" http://i.imgur.com/JwmiE.gif"];
-
-var result = Math.floor((Math.random() * links.length) + 0);
-
-let target = message.mentions.users.first() || message.author;
-
-await message.channel.send(`${target}`,{files: [
-    {   
-        attachment: links[result],
-        name: "meme.png"
-    }
-     ]});
-     if(!target) return message.reply(links[result]);
-    }
+}
     module.exports.help = {
         name: "shoot"
     }
