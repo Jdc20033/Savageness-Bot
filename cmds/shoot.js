2850750fc189f660e76d411756fe74f58e0e2c0c
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-30 20:25:49 -0500

    Updates

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -1,11 +1,23 @@
 module.exports.run = async(bot, message, args) => {
-    const {MessageAttachment} = require('discord.js');
-    const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+    let target = message.mentions.users.first() || message.author;
 
-    message.channel.send(attachment);
-
-
-}
-    module.exports.help = {
-        name: "shoot"
+    const links = [" https://media.giphy.com/media/VdcYTcb2nRSIU/giphy.gif",
+    " http://media.giphy.com/media/4cz3D8Yhyzcg8/giphy.gif",
+    " http://media.giphy.com/media/cS9lGF8gIBdQs/giphy.gif",
+    " http://media.giphy.com/media/MZDXTFeePBia4/giphy.gif",
+    " http://media.giphy.com/media/9umH7yTO8gLYY/giphy.gif",
+    " http://media.giphy.com/media/RXbc4wDSqYs3m/giphy.gif",
+    " http://media.giphy.com/media/FEq9xWq9fauLC/giphy.gif",
+    " https://i.makeagif.com/media/5-08-2014/ircXAq.gif",
+    " https://i.makeagif.com/media/7-29-2014/9_vBku.gif",
+    " http://i.imgur.com/IoJ4AGS.gif",
+    " http://i.imgur.com/JwmiE.gif"];
+    
+    var result = Math.floor((Math.random() * links.length) + 0);
+    
+            message.channel.send(target +  links[result]);
+    
     }
+        module.exports.help = {
+            name: "shoot"
+        }
