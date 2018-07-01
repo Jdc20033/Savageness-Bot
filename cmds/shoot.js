c77ced49ba64cfcc1249838360cc4007ca9cdae3
Jdc20033 <35044156+Jdc20033@users.noreply.github.com>
2018-06-30 20:23:22 -0500

    Updates

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -1,21 +1,6 @@
 module.exports.run = async(bot, message, args) => {
-    const {MessageAttachment } = require('discord.js');
-    let target = message.mentions.users.first() || message.author;
-    
-    const links = [" https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif",
-    " https://media.giphy.com/media/CZpro4AZHs436/giphy.gif"];
-    
-
-    const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+    const {MessageAttachment} = require('discord.js');
+    const attachment = MessageAttachment('https://i.imgur.com/w3duR07.png');
 
     message.channel.send(attachment);
 
